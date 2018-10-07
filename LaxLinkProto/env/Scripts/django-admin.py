3a34c0e6c907b6b6bfb9a253216640f0ac89b987
John <john.winkeler@gmail.com>
2018-10-07 12:39:53 -0500

    Add project files.

---
+++ LaxLinkProto/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Visual Studio 2017\Projects\LaxLinkProto\LaxLinkProto\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
